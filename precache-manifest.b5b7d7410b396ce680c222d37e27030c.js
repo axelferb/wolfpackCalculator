20a403a73c2639ab44cdcbc86f64ad38232df4f4
Axel <axel.ferb@gmail.com>
2020-05-13 23:10:08 +0200

    Updates

---
+++ precache-manifest.b5b7d7410b396ce680c222d37e27030c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13089f2af702080a170dbb81a1c5e77d",
+    "url": "/wolfpackCalculator/index.html"
+  },
+  {
+    "revision": "5a9f86450af85db66dc1",
+    "url": "/wolfpackCalculator/static/css/main.45dd3c87.chunk.css"
+  },
+  {
+    "revision": "3d4162705c347efe0e21",
+    "url": "/wolfpackCalculator/static/js/2.4c129853.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wolfpackCalculator/static/js/2.4c129853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a9f86450af85db66dc1",
+    "url": "/wolfpackCalculator/static/js/main.832800d4.chunk.js"
+  },
+  {
+    "revision": "94e0678c2d08d7d8a68b",
+    "url": "/wolfpackCalculator/static/js/runtime-main.34d4ea89.js"
+  }
+]);
